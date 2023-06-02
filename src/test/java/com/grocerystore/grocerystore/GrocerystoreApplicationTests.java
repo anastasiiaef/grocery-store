6362499cac2c4d0c3b040927dad9c88a7b124040
anastasiiaef <108550644+anastasiiaef@users.noreply.github.com>
2023-06-01 21:27:00 -0400

    Update .gitignore

---
+++ src/test/java/com/grocerystore/grocerystore/GrocerystoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grocerystore.grocerystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrocerystoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
