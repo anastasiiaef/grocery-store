6c4c4eab2fa98bdb4af3674a2b1e4f6e3ceeb3f2
anastasiiaef <108550644+anastasiiaef@users.noreply.github.com>
2023-06-01 21:06:07 -0400

    Test

---
+++ src/main/java/com/grocerystore/GrocerystoreApplication.java
@@ -0,0 +1,13 @@
+package com.grocerystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocerystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrocerystoreApplication.class, args);
+	}
+
+}
