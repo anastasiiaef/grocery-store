c83220ffe9b42e7f8f15ef4ff2954d880b773d05
anastasiiaef <108550644+anastasiiaef@users.noreply.github.com>
2023-06-16 11:27:33 -0400

    @RequestMapping added

--- src/main/java/com/grocerystore/GroceryStoreController.java
+++ src/main/java/com/grocerystore/GroceryStoreController.java
@@ -1,18 +1,55 @@
 package com.grocerystore;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class GroceryStoreController {
 
 	/**
-	 * Test Test
+	 * Handle the /home endpoint.
 	 * @return
 	 */
-	@RequestMapping("/start")
-	public String start() {
-		return "start";
+	@RequestMapping(value="/home", method=RequestMethod.GET)
+	public String read() {
+		return "home";
+	}
+	
+	/**
+	 * Handle the /home endpoint.
+	 * @return
+	 */
+	@RequestMapping(value="/home", method=RequestMethod.GET, headers= {"content-type=text/json"})
+	public String readJSON() {
+		return "home";
+	}
+		
+	/**
+	 * Handle the /home endpoint.
+	 * @return
+	 */
+	@PostMapping("/home")
+	public String create() {
+		return "home";
+	}
+
+	/**
+	 * Handle the /home endpoint.
+	 * @return
+	 */
+	@PutMapping("/home")
+	public String update() {
+		return "home";
+	}
+	
+	
+	@DeleteMapping("/home")
+	public String delete() {
+		return "home";
 	}
 
 }
