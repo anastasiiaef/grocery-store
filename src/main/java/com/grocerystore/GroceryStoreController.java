40432b6897b961e20454875a09bd0fbeb6e892be
bonima21 <79886034+bonima21@users.noreply.github.com>
2023-06-22 21:33:14 -0400

    model push
    Used model to push java objects

--- src/main/java/com/grocerystore/GroceryStoreController.java
+++ src/main/java/com/grocerystore/GroceryStoreController.java
@@ -2,11 +2,13 @@ package com.grocerystore;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.grocerystore.dto.GroceryDTO;
 import com.grocerystore.service.IGroceryService;
@@ -22,8 +24,9 @@ public class GroceryStoreController {
 	 * @return
 	 */
 	@RequestMapping(value="/home", method=RequestMethod.GET)
-	public String read() {
+	public String read(Model model) {
 		GroceryDTO groceryDTO = groceryServiceStub.fetchById(9);
+		model.addAttribute("groceryDTO", groceryDTO);
 		return "home";
 	}
 	
@@ -34,6 +37,16 @@ public class GroceryStoreController {
 	@RequestMapping(value="/home", method=RequestMethod.GET, headers= {"content-type=text/json"})
 	public String readJSON() {
 		return "home";
+	}
+	
+	@RequestMapping(value="/home", method=RequestMethod.GET, params= {"loyalty=silver"})
+	public ModelAndView readSilver() {
+		GroceryDTO groceryDTO = groceryServiceStub.fetchById(9);
+		groceryDTO.setGroceryId(80);
+		ModelAndView modelAndView=new ModelAndView();
+		modelAndView.setViewName("home");
+		modelAndView.addObject("groceryDTO", groceryDTO);
+		return modelAndView;
 	}
 		
 	/**
