2553cc134d79ebadc1897f62fc7cab932ab15f62
anastasiiaef <108550644+anastasiiaef@users.noreply.github.com>
2023-06-26 21:11:10 -0400

    @RespondBody added

--- src/main/java/com/grocerystore/GroceryStoreController.java
+++ src/main/java/com/grocerystore/GroceryStoreController.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.grocerystore;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -68,3 +69,99 @@ public class GroceryStoreController {
 	}
 
 }
+=======
+package com.grocerystore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.grocerystore.dto.GroceryDTO;
+import com.grocerystore.service.IGroceryService;
+
+@Controller
+public class GroceryStoreController {
+	
+	@Autowired
+	private IGroceryService groceryServiceStub;
+	
+	/**
+	 * Handle the /home endpoint.
+	 * @return
+	 */
+	@RequestMapping(value="/home", method=RequestMethod.GET)
+	@ResponseBody
+	public GroceryDTO read(Model model) {
+		GroceryDTO groceryDTO = groceryServiceStub.fetchById(9);
+		model.addAttribute("groceryDTO", groceryDTO);
+		return groceryDTO;
+	}
+	
+	/**
+	 * Handle the /home endpoint.
+	 * @return
+	 */
+	@RequestMapping(value="/home", method = RequestMethod.GET, params= {"apple"})
+	public ModelAndView readGrocery() {
+		GroceryDTO groceryDTO = groceryServiceStub.fetchById(9);
+		groceryDTO.setName("apple");
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("home");
+		modelAndView.addObject("groceryDTO", groceryDTO);
+		return modelAndView;
+	}
+	
+	
+	/**
+	 * Handle the /home endpoint.
+	 * @return
+	 */
+	@RequestMapping(value="/addgrocery", method = RequestMethod.GET)
+	public GroceryDTO addGrocery(Model model) {
+		GroceryDTO groceryDTO = groceryServiceStub.fetchById(9);
+		model.addAttribute("groceryDTO", groceryDTO);
+		return groceryDTO;
+	}
+	
+	
+	/**
+	 * Handle the /home endpoint.
+	 * @return
+	 */
+	@RequestMapping(value="/home", method=RequestMethod.GET, headers= {"content-type=text/json"})
+	public String readJSON() {
+		return "home";
+	}
+		
+	/**
+	 * Handle the /home endpoint.
+	 * @return
+	 */
+	@PostMapping("/home")
+	public String create() {
+		return "home";
+	}
+
+	/**
+	 * Handle the /home endpoint.
+	 * @return
+	 */
+	@PutMapping("/home")
+	public String update() {
+		return "home";
+	}
+	
+	@DeleteMapping("/home")
+	public String delete() {
+		return "home";
+	}
+
+}
+>>>>>>> Stashed changes
--- src/main/java/com/grocerystore/service/GroceryServiceStub.java
+++ src/main/java/com/grocerystore/service/GroceryServiceStub.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.grocerystore.service;
 
 import org.springframework.stereotype.Component;
@@ -21,3 +22,28 @@ public class GroceryServiceStub implements IGroceryService {
 		
 	}
 }
+=======
+package com.grocerystore.service;
+
+import org.springframework.stereotype.Component;
+
+import com.grocerystore.dto.GroceryDTO;
+
+@Component
+public class GroceryServiceStub implements IGroceryService {
+	
+	@Override
+	public GroceryDTO fetchById(int id) {
+		GroceryDTO groceryDTO = new GroceryDTO();
+		groceryDTO.setGroceryId(9);
+		groceryDTO.setName("Banana");
+		groceryDTO.setDescription("Fruit");
+		return groceryDTO;
+	}
+
+	@Override
+	public void save(GroceryDTO groceryDTO) {
+		
+	}
+}
+>>>>>>> Stashed changes
